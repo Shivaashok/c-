83e224a903d627889e3d086774316c923520673a
Shivaashok <122215006+Shivaashok@users.noreply.github.com>
2023-03-30 22:41:59 +0530

    Add files via upload

---
+++ day 3 qn 4.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+intmain()
+{
+   int x = 10;
+   int y = seventy;
+ 
+   x = x + y;
+   y = x - y;
+   x = x - y;
+ 
+   cout<< "X : "<< x << "\n";
+   cout<< "Y : "<< y << "\n";
+    
+   return0;
+}
+for the int variable y it is mentioned "seventy " instead of 70
+
